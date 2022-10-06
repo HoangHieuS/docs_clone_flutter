098aebfb33245c58b3697c956475fb0504c7295f
HoangHieu <dinhhoanghieu321@gmail.com>
2022-10-06 21:05:33 +0700

    Auth Middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const auth = require("../middlewares/auth");
 
 const authRouter = express.Router();
 
@@ -18,10 +20,17 @@ authRouter.post("/api/signup", async (req, res) => {
       user = await user.save();
     }
 
-    res.status(200).json({ user });
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+
+    res.status(200).json({ user, token });
   } catch (e) {
-    res.status(500).json({error: e.message});
+    res.status(500).json({ error: e.message });
   }
+});
+
+authRouter.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({ user, token: req.token });
 });
 
 module.exports = authRouter;
