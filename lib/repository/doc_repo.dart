d5e628ee716c947a9f0291ab75f7aa203da04834
HoangHieu <dinhhoanghieu321@gmail.com>
2022-10-10 21:43:41 +0700

    Designing Document Screen & Updating Document title

--- lib/repository/doc_repo.dart
+++ lib/repository/doc_repo.dart
@@ -100,4 +100,65 @@ class DocRepo {
     }
     return error;
   }
+
+  void updateDocTitle({
+    required String token,
+    required String id,
+    required String title,
+  }) async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occurred.',
+      data: null,
+    );
+    try {
+      await _client.post(
+        Uri.parse('$host/doc/title'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        },
+        body: jsonEncode({
+          'id': id,
+          'title': title,
+        }),
+      );
+    } catch (e) {
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
+    }
+  }
+
+  Future<ErrorModel> getDocById(String token, String id) async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occurred.',
+      data: null,
+    );
+    try {
+      var res = await _client.get(
+        Uri.parse('$host/doc/$id'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        },
+      );
+      switch (res.statusCode) {
+        case 200:
+          error = ErrorModel(
+            error: null,
+            data: DocumentModel.fromJson(res.body),
+          );
+          break;
+        default:
+          throw 'This Document does not exist, please create a new one.';
+      }
+    } catch (e) {
+      error = ErrorModel(
+        error: e.toString(),
+        data: null,
+      );
+    }
+    return error;
+  }
 }
--- lib/screens/doc_screen.dart
+++ lib/screens/doc_screen.dart
@@ -1,19 +1,145 @@
+import 'package:docs_clone_flutter/colors.dart';
+import 'package:docs_clone_flutter/repository/auth_repo.dart';
+import 'package:docs_clone_flutter/repository/doc_repo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/models.dart';
 
 class DocScreen extends ConsumerStatefulWidget {
   final String id;
-  const DocScreen({super.key, required this.id,});
+  const DocScreen({
+    super.key,
+    required this.id,
+  });
 
   @override
   ConsumerState<DocScreen> createState() => _DocScreenState();
 }
 
 class _DocScreenState extends ConsumerState<DocScreen> {
+  final TextEditingController _titleController =
+      TextEditingController(text: 'Untitled Document');
+  final quill.QuillController _controller = quill.QuillController.basic();
+  ErrorModel? errorModel;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDocumentData();
+  }
+
+  void fetchDocumentData() async {
+    errorModel = await ref.read(docRepoProvider).getDocById(
+          ref.read(userProvider)!.token,
+          widget.id,
+        );
+
+    if (errorModel!.data != null) {
+      _titleController.text = (errorModel!.data as DocumentModel).title;
+      setState(() {
+        
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void updateDocTitle(WidgetRef ref, String title) {
+    ref.read(docRepoProvider).updateDocTitle(
+          token: ref.read(userProvider)!.token,
+          id: widget.id,
+          title: title,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text(widget.id),),
-    );
+        appBar: AppBar(
+          backgroundColor: kWhiteColor,
+          elevation: 0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.lock,
+                  size: 16,
+                ),
+                label: const Text('Share'),
+                style: ElevatedButton.styleFrom(backgroundColor: kBlueColor),
+              ),
+            ),
+          ],
+          title: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/docs-logo.png',
+                  height: 40,
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: 180,
+                  child: TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kBlueColor),
+                      ),
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                    onSubmitted: (value) => updateDocTitle(ref, value),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: kGreyColor,
+                  width: 0.1,
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              quill.QuillToolbar.basic(controller: _controller),
+              const SizedBox(height: 10),
+              Expanded(
+                child: SizedBox(
+                  width: 750,
+                  child: Card(
+                    color: kWhiteColor,
+                    elevation: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: quill.QuillEditor.basic(
+                        controller: _controller,
+                        readOnly: false,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
